d89bd38843c0f23d189d1ee46315b4f190293a3d
dimalka-heshan <dimalkarathnayake10@gmail.com>
2023-01-12 22:37:36 +0530

    genre routes

--- server/routes/genre.routes.js
+++ server/routes/genre.routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const GenreRouter = express.Router();
 
 const {
-//   GetAllGenre,
+  GetAllGenres,
   CreateGenre,
 } = require("../controllers/genre.controller");
 
-// GenreRouter.get("/", GetAllGenre);
+GenreRouter.get("/", GetAllGenres);
 GenreRouter.post("/create", CreateGenre);
 
 module.exports = GenreRouter;
